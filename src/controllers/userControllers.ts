0f6c9dde1a0eebbf8385baed759520d0a8c90d5b
Amit Carpenter 01 <amitcarpenter199@gmail.com>
2024-06-20 11:59:14 +0530

    phone nuber not required

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -18,7 +18,7 @@ export const register = async (req: Request, res: Response) => {
     const registerSchema = Joi.object({
       name: Joi.string().min(3).max(30).required(),
       email: Joi.string().email().required(),
-      phone: Joi.string().min(10).max(15).required(),
+      // phone: Joi.string().min(10).max(15).required(),
       password: Joi.string().min(8).required()
     });
 
