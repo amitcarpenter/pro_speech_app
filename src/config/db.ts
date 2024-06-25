c39b75b01a17f3da51f0e3b7320a7a71286f163e
Amit Carpenter 01 <amitcarpenter199@gmail.com>
2024-06-25 11:49:10 +0530

    databse change

--- src/config/db.ts
+++ src/config/db.ts
@@ -6,12 +6,12 @@ const DB_URI = process.env.DB_URI as string;
 
 const connect_mongodb = async (): Promise<void> => {
   try {
-    await mongoose.connect(DB_URI);
+    await mongoose.connect(DB_URI, { dbName: "pro_speech_app" });
     console.log("Database Connected SuccessFully");
   } catch (error) {
     console.log("Database Connection Error", error);
     process.exit(1);
   }
-};
+};  
 
 export default connect_mongodb;
