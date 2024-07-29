a8d702bcf84b4040d8cf0383eb793741c159766d
Amit Carpenter 01 <amitcarpenter199@gmail.com>
2024-07-29 13:24:58 +0530

    ssl add

--- app.ts
+++ app.ts
@@ -3,14 +3,25 @@ import mongodb_connection from "./src/config/db";
 import configureApp from "./src/config/routes"
 import dotenv from "dotenv";
 import path from 'path';
+import https from "https";
+import fs from "fs";
+
+const PORT = process.env.PORT as string;
+const APP_URL = process.env.APP_URL as string;
+const EXPRESS_SESSION_SECRET = process.env.EXPRESS_SESSION_SECRET as string;
+
 dotenv.config()
 
 mongodb_connection();
 const app: Application = express();
 
-const PORT = process.env.PORT as string;
-const APP_URL = process.env.APP_URL as string;
-const EXPRESS_SESSION_SECRET = process.env.EXPRESS_SESSION_SECRET as string;
+const sslOptions = {
+  ca: fs.readFileSync("/var/www/html/ssl/ca_bundle.crt"),
+  key: fs.readFileSync("/var/www/html/ssl/private.key"),
+  cert: fs.readFileSync("/var/www/html/ssl/certificate.crt"),
+};
+// Create HTTPS server
+const httpsServer = https.createServer(sslOptions, app);
 
 
 app.use('/', express.static(path.join(__dirname, 'src/uploads')));
@@ -26,6 +37,10 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 
-app.listen(PORT, (): void => {
+// app.listen(PORT, (): void => {
+//   console.log(`Server is working on ${APP_URL}`);
+// });
+
+httpsServer.listen(PORT, () => {
   console.log(`Server is working on ${APP_URL}`);
-});
+})
